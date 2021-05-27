9fa5e118ae8b3cc25ed2e98a302248ef709728f5
Serhii B <s_blyshchak@ukr.net>
2021-05-27 21:01:32 +0300

    Create app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Redirect, Route, Switch } from 'react-router-dom'
+import './App.css'
+import HomePage from './pages/home/HomePage';
+import RatePage from './pages/rate/RatePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Switch>
+          <Route exact path='/' render={ () => <Redirect to={'/home'} /> }/>
+          <Route path='/home' render={ () => <HomePage/> } />
+          <Route path='/rate' render={ () => <RatePage/> } />
+          <Route path='*' render={ () => <div>404 NOT FOUND</div>} />
+        </Switch>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/pages/rate/RatePage.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const RatePage = () => {
+    return (
+        <div>
+            rate
+        </div>
+    )
+}
+
+export default RatePage
